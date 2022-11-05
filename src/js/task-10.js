7f351a4fbff1d8b818f7cab3039fea66fea9e603
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-05 23:16:18 +0200

    performance work 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,33 @@
+const destroyBtn = document.querySelector('button[data-destroy]');
+const divBoxesRef = document.querySelector('#boxes');
+const inputRef = document.querySelector('#controls > input');
+const createBtnRef = document.querySelector('button[data-create]');
+const arrDivs = [];
+let size = 10;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    console.log(amount);
+    size += 10;
+    let div = document.createElement('div');
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    arrDivs.push(div);
+  }
+  divBoxesRef.append(...arrDivs);
+}
+
+createBtnRef.addEventListener('click', create => {
+  const value = inputRef.value;
+  createBoxes(value);
+});
+
+destroyBtn.addEventListener('click', clear => {
+  divBoxesRef.innerHTML = '';
+  inputRef.value = '';
+});
