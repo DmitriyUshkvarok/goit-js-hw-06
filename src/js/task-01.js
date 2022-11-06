0499a2e5c79cb93c99b1513809ce87764303fdc4
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-06 13:48:41 +0200

    fix

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const fullList = document.querySelectorAll('.item');
-console.log(`Number of categories: ${fullList.length}`);
-
-fullList.forEach(items => {
-  console.log(`Elements: ${items.firstElementChild.textContent}`);
-  console.log(`Category: ${items.lastElementChild.children.length}`);
-});
